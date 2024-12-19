210459d57cc10a59ab8be0cf456203f94653b718
ci010 <ci010@users.noreply.github.com>
2024-12-19 17:16:58 +0000

    Bump xmcl to v0.48.0

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,31 +1,31 @@
 cask "xmcl" do
-  version "0.47.15"
-  sha256 :no_check
-  arch arm: "arm64", intel: "x86_64"
+version "0.48.0"
+sha256 :no_check
+arch arm: "arm64", intel: "x86_64"
 
-  on_arm do 
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.47.15/xmcl-0.47.15-arm64.dmg"
-  end
+on_arm do 
+  url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.48.0/xmcl-0.48.0-arm64.dmg"
+end
 
-  on_intel do
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.47.15/xmcl-0.47.15-x64.dmg"
-  end
+on_intel do
+  url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.48.0/xmcl-0.48.0-x64.dmg"
+end
 
-  name "X Minecraft Launcher"
-  desc "An Open Source Minecraft Launcher with Modern UX. Provides a Disk Efficient way to manage all your Mods!"
-  homepage "https://xmcl.app"
+name "X Minecraft Launcher"
+desc "An Open Source Minecraft Launcher with Modern UX. Provides a Disk Efficient way to manage all your Mods!"
+homepage "https://xmcl.app"
 
-  livecheck do
-    url :url
-    strategy :header_match
-  end
-  
-  app "X Minecraft Launcher.app"
+livecheck do
+  url :url
+  strategy :header_match
+end
 
-  zap trash: [
-    "~/Applications/X Minecraft Launcher.app",
-    "~/Library/Application Support/xmcl",
-    "~/Library/Preferences/xmcl.plist",
-    "~/Library/Saved Application State/xmcl.savedState"
-  ]
+app "X Minecraft Launcher.app"
+
+zap trash: [
+  "~/Applications/X Minecraft Launcher.app",
+  "~/Library/Application Support/xmcl",
+  "~/Library/Preferences/xmcl.plist",
+  "~/Library/Saved Application State/xmcl.savedState"
+]
 end
