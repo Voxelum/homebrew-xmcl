876f9e9107bfb0bb8cdf67b899b3120a383b757d
CI010 <cijhn@hotmail.com>
2024-12-21 02:34:52 +0800

    Bump xmcl to v0.48.1 (#2)
    Co-authored-by: ci010 <ci010@users.noreply.github.com>

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,14 +1,14 @@
 cask "xmcl" do
-  version "0.47.15"
+  version "0.48.1"
   sha256 :no_check
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do 
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.47.15/xmcl-0.47.15-arm64.dmg"
+    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.48.1/xmcl-0.48.1-arm64.dmg"
   end
 
   on_intel do
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.47.15/xmcl-0.47.15-x64.dmg"
+    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.48.1/xmcl-0.48.1-x64.dmg"
   end
 
   name "X Minecraft Launcher"
