9dc60be0c060c3d291107fb999edae3950bbd952
CI010 <cijhn@hotmail.com>
2025-07-27 13:59:42 +0800

    Bump xmcl to v0.51.2 (#24)
    Co-authored-by: ci010 <ci010@users.noreply.github.com>

--- Casks/xmcl.rb
+++ Casks/xmcl.rb
@@ -1,14 +1,14 @@
 cask "xmcl" do
-  version "0.51.1"
+  version "0.51.2"
   sha256 :no_check
   arch arm: "arm64", intel: "x86_64"
 
   on_arm do 
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.51.1/xmcl-0.51.1-arm64.dmg"
+    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.51.2/xmcl-0.51.2-arm64.dmg"
   end
 
   on_intel do
-    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.51.1/xmcl-0.51.1-x64.dmg"
+    url "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v0.51.2/xmcl-0.51.2-x64.dmg"
   end
 
   name "X Minecraft Launcher"
